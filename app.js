4c6eb7945c224469c3b6fce0b5b1d9014c155505
Emmanuelle Laguna Marin <emmanuelle.laguna@external.danone.com>
2025-01-12 08:07:58 -0600

    REFACTOR: express config

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 
 var app = express();
 
+app.get("/", (req, res) => res.send("Tailwind Course"));
+
 var staticPath = path.join(__dirname, "/HTML");
 app.use(express.static(staticPath));
 
